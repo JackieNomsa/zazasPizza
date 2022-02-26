45488de8dff3050e28cdc21a662d1ccd5c406894
JackieNomsa <jackienomsa3@gmail.com>
2022-02-26 17:10:26 +0200

    pizza

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,6 +5,9 @@ const Home = () => {
     return (
         <>
         <div className="home"></div>
+        <div className="pizza">
+            <img src="https://themes.muffingroup.com/be/pizza/wp-content/uploads/2014/09/home_pizza_slider_1.png"></img>
+        </div>
         <div className="menu-location">
             <div>
             <MdMenuBook className="menu"/>
