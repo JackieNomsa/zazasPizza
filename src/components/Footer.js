84d613383526f7cf9f0cc13f006bb1dd4490b46d
JackieNomsa <jackienomsa3@gmail.com>
2022-03-04 10:51:17 +0200

    menu

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,9 +3,11 @@ import React from "react";
 const Footer = () => {
     return <>
         <div className="footer">
-        <p>Terms & Conditions</p>
-        <p>Logo</p>
-        <p>&copy; Copyright Reserved Jackie-Nomsa 2021
+            <div className="first">
+            <p>Terms & Conditions</p>
+            <p>Logo</p>
+            </div>
+        <p className="copy">&copy; Copyright Reserved Jackie-Nomsa 2021
         </p>
         </div>
     </>
--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,9 +1,18 @@
 import React from "react";
+import { BiDrink } from "react-icons/bi";
 
 const Menu = () => {
     return <>
+    <div className="main-menu">
+    <div className="menu-page">
         <h1>Menu</h1>
         <button>Download Menu</button>
+    </div>
+    {/* add menu here */}
+    <div>
+        <BiDrink className="drinks"/>
+    </div>
+    </div>  
     </>
 }
 
